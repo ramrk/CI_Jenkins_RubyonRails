faabefad26896776a6994ae02c7a3024e470a07f
rama_krishna <rk.ramakrishna11@gmail.com>
2020-02-11 15:18:25 +0530

    second commit

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,3 @@
 require 'simplecov'
 SimpleCov.start
-require_relative '/var/lib/jenkins/workspace/CI_RubyOnRails/app/controllers/string_ops'
+require_relative '/var/lib/jenkins/workspace/CI_Jenkins_RubyonRails/app/controllers/string_ops'
